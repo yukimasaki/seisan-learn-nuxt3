dfa3f7abd2a86778b2f23261390f67f4c30ffbaf
yukimasaki <woodenhoe@gmail.com>
2023-08-03 08:22:00 +0000

    feat: 各ページへのリンクを配置

--- src/seisan/pages/index.vue
+++ src/seisan/pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    Index Page
+  <div class="p-4">
+    <ul class="list-disc list-inside">
+      <li><NuxtLink to="/login">Login</NuxtLink></li>
+      <li><NuxtLink to="/welcome">Welcome</NuxtLink></li>
+      <li><NuxtLink to="/graph">Graph</NuxtLink></li>
+      <li><NuxtLink to="/list">List</NuxtLink></li>
+      <li><NuxtLink to="/setting">Setting</NuxtLink></li>
+    </ul>
   </div>
 </template>
