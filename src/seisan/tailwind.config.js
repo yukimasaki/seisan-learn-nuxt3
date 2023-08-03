474fa9e24b15d7e93070da21fb7d69c6cd32464c
yukimasaki <woodenhoe@gmail.com>
2023-08-03 05:20:55 +0000

    feat: tailwind.config > テーマカラーを追加

--- src/seisan/tailwind.config.js
+++ src/seisan/tailwind.config.js
@@ -1,4 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const customColor = {
+  primary: '#5f939a',
+  secondary: "#1b2021",
+  accent: "#d8ac9c",
+  neutral: "#eac8af",
+  base: "#eff4f4",
+  info: "#4e8dda",
+  success: "#1a7465",
+  warning: "#9f730f",
+  error: "#f33f42",
+}
+
 module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
@@ -9,8 +21,35 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "daisy-primary": customColor.primary,
+      },
+      screens: {
+        sm: { 'max': '560px' },
+        md: { 'max': '768px' },
+        tb: { 'max': '960px' },
+      }
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        aifuku: {
+          "primary": customColor.primary,
+          "secondary": customColor.secondary,
+          "accent": customColor.accent,
+          "neutral": customColor.neutral,
+          "base-100": customColor.base,
+          "info": customColor.info,
+          "success": customColor.success,
+          "warning": customColor.warning,
+          "error": customColor.error,
+        }
+      }
+    ]
+  },
 }
-
