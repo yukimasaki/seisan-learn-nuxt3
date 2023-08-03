2f52abd73088b93cf50cf5d9c9c45b8581370e87
yukimasaki <woodenhoe@gmail.com>
2023-08-03 05:23:50 +0000

    feat: app.vueにテンプレート系タグを移動

--- src/seisan/app.vue
+++ src/seisan/app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtPage />
+  <div class="flex h-screen overflow-hidden">
+    <Sidebar></Sidebar>
+    <div class="relative flex-1 flex-col overflow-y-auto overflow-x-hidden">
+      <Header></Header>
+      <NuxtPage></NuxtPage>
+      <!-- フッター -->
+      <Footer></Footer>
+    </div>
   </div>
 </template>
--- src/seisan/pages/index.vue
+++ src/seisan/pages/index.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="flex h-screen overflow-hidden">
-    <Sidebar></Sidebar>
-    <div class="relative flex-1 flex-col overflow-y-auto overflow-x-hidden">
-      <Header></Header>
-      <!-- メインコンテンツ -->
-      <!-- フッター -->
-      <Footer></Footer>
-    </div>
+  <div>
+    Index Page
   </div>
 </template>
