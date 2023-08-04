76a14bb328e7cd90510fbbae5256983fed768ee0
yukimasaki <woodenhoe@gmail.com>
2023-08-04 08:44:14 +0000

    feat: ヘッダーをdaisyUIのnavbarに置き換え

--- src/seisan/layouts/default.vue
+++ src/seisan/layouts/default.vue
@@ -2,8 +2,7 @@
   <div class="flex h-screen overflow-hidden">
     <Sidebar></Sidebar>
     <div class="relative flex-1 flex-col overflow-y-auto overflow-x-hidden">
-      <!-- <HeaderLoggedIn></HeaderLoggedIn> -->
-      <HeaderLoggedInNew></HeaderLoggedInNew>
+      <HeaderLoggedIn></HeaderLoggedIn>
       <slot />
       <Footer></Footer>
     </div>
