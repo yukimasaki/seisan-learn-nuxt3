fecf0b6056e9ab2fa596178bbc9b7031f2cfa8da
yukimasaki <woodenhoe@gmail.com>
2023-08-04 08:07:26 +0000

    feat: ヘッダーを作り直し中

--- src/seisan/layouts/default.vue
+++ src/seisan/layouts/default.vue
@@ -2,7 +2,8 @@
   <div class="flex h-screen overflow-hidden">
     <Sidebar></Sidebar>
     <div class="relative flex-1 flex-col overflow-y-auto overflow-x-hidden">
-      <HeaderLoggedIn></HeaderLoggedIn>
+      <!-- <HeaderLoggedIn></HeaderLoggedIn> -->
+      <HeaderLoggedInNew></HeaderLoggedInNew>
       <slot />
       <Footer></Footer>
     </div>
