2924afb8433badd6781f9074481e82cdfa190ec0
yukimasaki <woodenhoe@gmail.com>
2023-08-08 08:46:42 +0000

    feat: 年月ピッカーのデザイン・ロジック実装中

--- src/seisan/components/month-picker.vue
+++ src/seisan/components/month-picker.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div onclick="my_month_picker.showModal()">
-      <span>2023</span>
-      <span>年</span>
-      <span>8</span>
-      <span>月</span>
+    <div
+      onclick="my_month_picker.showModal()"
+      class="btn btn-ghost"
+    >
+      {{ now.format('YYYY年M月') }}
     </div>
 
     <dialog id="my_month_picker" class="modal">
@@ -17,12 +17,14 @@
         </div>
 
         <!-- ボディ部分 -->
-        <div class="gird grid-cols-4 gap-1">
-          <div
+        <div class="grid grid-cols-4 gap-1 ">
+          <span
             v-for="month in months"
+            class="self-center justify-self-center"
+            @click="onSelectMonthPicker()"
           >
-            {{ month }}
-          </div>
+            {{ month }}月
+          </span>
         </div>
       </form>
       <form method="dialog" class="modal-backdrop">
@@ -33,5 +35,11 @@
 </template>
 
 <script setup lang="ts">
+import dayjs, { Dayjs } from 'dayjs';
+
+const now = ref(dayjs());
+const selectedYearMonth: Ref<Dayjs | undefined> = ref();
 const months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+
+const onSelectMonthPicker = () => {}
 </script>
