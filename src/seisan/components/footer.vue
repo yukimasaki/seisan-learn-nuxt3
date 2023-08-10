c7945d6ef7e8de4ad9c87f36c2c6e9e27118eb2d
yukimasaki <woodenhoe@gmail.com>
2023-08-10 08:10:39 +0000

    feat: フッターに丸ボタンを実装

--- src/seisan/components/footer.vue
+++ src/seisan/components/footer.vue
@@ -1,20 +1,38 @@
 <template>
-  <div class="sticky bottom-0 z-30 p-4 tb:p-2 flex w-full bg-base-100 drop-shadow">
+  <div class="sticky bottom-0 z-30 p-4 tb:p-2 flex w-full drop-shadow">
     <div class="flex flex-grow items-center justify-between ps-6 pe-6">
-      <span>家</span>
-      <span>筆</span>
-      <span>覧</span>
+      <button
+        class="btn btn-circle drop-shadow"
+        @click="navigateToHome()"
+      >
+        <IconBaselineHome></IconBaselineHome>
+      </button>
+      <button
+        class="btn btn-circle drop-shadow"
+        @click="add()"
+      >
+        <IconPencil></IconPencil>
+      </button>
+      <button
+        class="btn btn-circle drop-shadow"
+        @click="navigateToList()"
+      >
+        <IconBaselineFormatListBulleted></IconBaselineFormatListBulleted>
+      </button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const navigateToHome = () => {
+  return navigateTo('/');
 }
 
 const add = () => {
+  console.log(`モーダルを表示する`);
 }
 
 const navigateToList = () => {
+  return navigateTo('/list');
 }
 </script>
