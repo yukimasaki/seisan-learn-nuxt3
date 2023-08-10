fab27013db5f6613f323c7c15cc7e0a72259bf4d
yukimasaki <woodenhoe@gmail.com>
2023-08-10 05:41:19 +0000

    faet: 年月ピッカー実装

--- src/seisan/components/month-picker.vue
+++ src/seisan/components/month-picker.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <div
-      onclick="my_month_picker.showModal()"
+      onclick="yearMonthPicker.showModal()"
       class="btn btn-ghost btn-xs"
     >
-      {{ currentYearMonth.format('YYYY年M月') }}
+      {{ headerLabel.format('YYYY年M月') }}
     </div>
 
-    <dialog id="my_month_picker" class="modal">
+    <dialog id="yearMonthPicker" class="modal" ref="yearMonthPicker">
       <form method="dialog" class="modal-box">
         <!-- ヘッダー部分 -->
         <div class="flex justify-between">
@@ -15,7 +15,7 @@
             <IconBaselineKeyboardArrowLeft></IconBaselineKeyboardArrowLeft>
           </div>
           <div class="btn btn-ghost">
-            {{ pickerYearMonth.format('YYYY') }}
+            {{ pickerLabel.format('YYYY') }}
           </div>
           <div class="btn btn-ghost" @click="navigateMonth('next')">
             <IconBaselineKeyboardArrowRight></IconBaselineKeyboardArrowRight>
@@ -43,26 +43,36 @@
 <script setup lang="ts">
 import dayjs, { Dayjs } from 'dayjs';
 
-const currentYearMonth: Ref<Dayjs> = useState('currentYearMonth');
+// カレンダー(親コンポーネント)のヘッダー部分に表示される年月ラベル
+const headerLabel: Ref<Dayjs> = useState('currentYearMonth');
 
+console.log(`年月ピッカー初期化時:`);
+console.log(headerLabel.value.format('YYYY/MM'));
+
+// 年月ピッカーを選択時に年月ラベルの値を更新する
 const onSelectMonthPicker = (month: Dayjs): void => {
-  currentYearMonth.value = month;
+  headerLabel.value = month;
 }
 
-const pickerYearMonth: Ref<Dayjs> = ref(dayjs());
-pickerYearMonth.value = currentYearMonth.value.clone();
+// 年月ピッカー(自コンポーネント)のヘッダー部分に表示される年ラベル
+const pickerLabel: Ref<Dayjs> = ref(dayjs());
+watch(headerLabel, (next, prev) => {
+  pickerLabel.value = headerLabel.value;
+});
 
+
+// 年月ピッカーの戻る進むボタンを押した時に年ラベルの表示内容を更新する
 const navigateMonth = (direction: string): void => {
   if (direction === 'prev') {
-    pickerYearMonth.value = dayjs(pickerYearMonth.value).subtract(1, 'year');
+    pickerLabel.value = dayjs(pickerLabel.value).subtract(1, 'year');
   } else {
-    pickerYearMonth.value = dayjs(pickerYearMonth.value).add(1, 'year');
+    pickerLabel.value = dayjs(pickerLabel.value).add(1, 'year');
   }
 }
 
 const months: ComputedRef<Dayjs[]> = computed(() => {
   return Array.from({ length: 12 }, (_, monthIndex) => {
-    return dayjs(pickerYearMonth.value).month(monthIndex).startOf('month');
+    return dayjs(pickerLabel.value).month(monthIndex).startOf('month');
   });
 });
 </script>
