194b7fa72d75a1f44877485b878e4a8dceab5001
yukimasaki <woodenhoe@gmail.com>
2023-08-10 06:39:07 +0000

    feat: フッターの実装を開始

--- src/seisan/components/footer.vue
+++ src/seisan/components/footer.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    Footer Component
+  <div class="sticky bottom-0 z-30 p-4 tb:p-2 flex w-full bg-base-100 drop-shadow">
+    <div class="flex flex-grow items-center justify-between ps-6 pe-6">
+      <span>家</span>
+      <span>筆</span>
+      <span>覧</span>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const navigateToHome = () => {
+}
+
+const add = () => {
+}
+
+const navigateToList = () => {
+}
+</script>
