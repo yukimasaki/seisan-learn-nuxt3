1b577fa3af54d3aef64086e065e17aa7ff2907f9
yukimasaki <woodenhoe@gmail.com>
2023-08-12 07:09:47 +0000

    feat: リストのデザイン調整

--- src/seisan/pages/list.vue
+++ src/seisan/pages/list.vue
@@ -14,10 +14,16 @@
         v-for="detail in details"
         :key="detail.id"
       >
-        <div class="px-8 py-1 flex justify-between border-b">
-          <span class="">ID: {{ detail.id }}</span>
-          <span class="">タイトル: {{ detail.title }}</span>
-          <span class="">金額: {{ detail.amount }}円</span>
+        <div class="px-4 py-1 border-b">
+          <div class="flex justify-between font-semibold">
+            <span>{{ detail.genre }}</span>
+            <span>{{ detail.amount }}円</span>
+          </div>
+          <div>
+            <span class="px-1 text-sm badge bg-green-600 text-green-50">{{ detail.paymentMethod }}</span>
+            <span class="px-1 text-sm">{{ detail.paymentDate.format('YYYY/MM/DD') }}</span>
+            <span class="px-1 text-sm">{{ detail.memo }}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -25,6 +31,7 @@
 </template>
 
 <script setup lang="ts">
+import dayjs from 'dayjs';
 import { randBetween } from '../utils/randBetween';
 
 definePageMeta({
@@ -33,7 +40,10 @@ definePageMeta({
 
 const details = Array.from({ length: 50 }, (_, index) => ({
   id: index + 1,
-  title: '食材',
+  genre: '光熱費',
   amount: randBetween(98, 19800),
+  paymentMethod: '比率',
+  paymentDate: dayjs(`${randBetween(2022, 2023)}-${randBetween(1, 12)}-${randBetween(1, 28)}`),
+  memo: '電気代',
 }));
 </script>
