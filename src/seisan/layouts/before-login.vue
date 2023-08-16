edd79c519e9aef039a123805de9deeac54822236
yukimasaki <woodenhoe@gmail.com>
2023-08-16 08:21:53 +0000

    feat: ログイン前のレイアウトからフッターを削除

--- src/seisan/layouts/before-login.vue
+++ src/seisan/layouts/before-login.vue
@@ -3,7 +3,6 @@
     <div class="relative flex-1 flex-col overflow-y-auto overflow-x-hidden">
       <HeaderBeforeLogin></HeaderBeforeLogin>
       <slot />
-      <Footer></Footer>
     </div>
   </div>
 </template>
