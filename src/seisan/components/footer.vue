75b8356ac934a86d668f6e5ed7f0e83ae868e082
yukimasaki <woodenhoe@gmail.com>
2023-08-18 02:26:27 +0000

    feat: 追加ボタンにレコードを追加する処理を追加

--- src/seisan/components/footer.vue
+++ src/seisan/components/footer.vue
@@ -24,12 +24,28 @@
 </template>
 
 <script setup lang="ts">
+import dayjs from 'dayjs';
+import { Detail } from '../types/detail';
+
 const navigateToHome = () => {
   return navigateTo('/');
 }
 
 const add = () => {
-  console.log(`モーダルを表示する`);
+  const detail: Detail = {
+    id: 51,
+    genre: '分類不能',
+    amount: 9999,
+    paymentMethod: '金額指定',
+    paymentDate: dayjs(`2023-09-01`),
+    memo: 'added',
+    paidStatus: false,
+    createdAt: dayjs(`2023-09-01`),
+  }
+
+  const details: Ref<Detail[]> = useState('details');
+  details.value.push(detail);
+  details.value.sort((a, b) => b.createdAt.unix() - a.createdAt.unix());
 }
 
 const navigateToList = () => {
