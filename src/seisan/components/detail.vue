32d9b16330bd85d505eb405384f4096c2d31f6a7
yukimasaki <woodenhoe@gmail.com>
2023-08-18 02:13:46 +0000

    feat: detailコンポーネントに表示するデータをストアから取得するよう修正

--- src/seisan/components/detail.vue
+++ src/seisan/components/detail.vue
@@ -19,39 +19,11 @@
 </template>
 
 <script setup lang="ts">
-import dayjs from 'dayjs';
-import { randBetween } from '../utils/randBetween';
+import { Detail } from '../types/detail';
 
-// サンプルデータ（バックエンドとの繋ぎ込みが完了したら削除する）
-const genre = [
-  '通信費',
-  '食費',
-  '外食費',
-  '住居費',
-  '光熱費',
-  '水道費',
-  '美容費',
-  '税金・保険費',
-  '分類不能',
-];
-
-const paymentMethod = [
-  '比率',
-  '金額',
-  '均等',
-];
-
-const details = reactive(Array.from({ length: 50 }, (_, index) => ({
-  id: index + 1,
-  genre: genre[randBetween(0, genre.length - 1)],
-  amount: randBetween(98, 19800).toLocaleString(),
-  paymentMethod: paymentMethod[randBetween(0, paymentMethod.length - 1)],
-  paymentDate: dayjs(`2023-08-${randBetween(1, 31)}`),
-  memo: genre[randBetween(0, genre.length - 1)],
-  paidStatus: randBetween(0, 1) ? true : false,
-  createdAt: dayjs(`2023-08-${randBetween(1, 31)}`),
-})));
+// ストアに格納されたデータを取得してくる
+const details: Ref<Detail[]> = useState('details');
 
 // detailsを降順にソート
-details.sort((a, b) => b.createdAt.unix() - a.createdAt.unix());
+details.value.sort((a, b) => b.createdAt.unix() - a.createdAt.unix());
 </script>
