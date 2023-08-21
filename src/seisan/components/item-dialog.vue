3e5c4f1187f938d778a17dee5b7802396e552135
yukimasaki <woodenhoe@gmail.com>
2023-08-21 08:50:55 +0000

    feat: アイテムダイアログの中身を仮配置

--- src/seisan/components/item-dialog.vue
+++ src/seisan/components/item-dialog.vue
@@ -25,16 +25,47 @@
 
         <!-- ボディ部分 -->
         <div class="flex flex-col">
+          <label class="my-2">金額</label>
+          <input
+            type="number"
+            placeholder="1980"
+            class="input input-bordered w-full focus:outline-none bg-stone-50"
+          />
 
+          <label class="my-2">カテゴリ</label>
+          <select class="select select-bordered w-full focus:outline-none bg-stone-50">
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+
+          <label class="my-2">日付</label>
+          <input
+            type="date"
+            class="input input-bordered w-full focus:outline-none bg-stone-50"
+          />
+
+          <label class="my-2">メモ</label>
+          <textarea class="textarea textarea-bordered w-full focus:outline-none bg-stone-50" />
+
+          <div class="flex justify-between my-2">
+            <label class="my-auto">割り勘方法</label>
+            <div class="join my-auto">
+              <input type="radio" class="join-item btn btn-xs" name="options" aria-label="均等">
+              <input type="radio" class="join-item btn btn-xs" name="options" aria-label="比率">
+              <input type="radio" class="join-item btn btn-xs" name="options" aria-label="金額">
+              <input type="radio" class="join-item btn btn-xs" name="options" aria-label="なし">
+            </div>
+          </div>
         </div>
 
         <!-- フッター部分 -->
         <div class="modal-action justify-center">
-          <button class="btn w-1/2">
+          <button class="btn w-1/2 drop-shadow">
             キャンセル
           </button>
 
-          <button class="btn w-1/2">
+          <button class="btn w-1/2 btn-disabled drop-shadow">
             作成
           </button>
         </div>
@@ -44,5 +75,4 @@
 </template>
 
 <script setup lang="ts">
-
 </script>
