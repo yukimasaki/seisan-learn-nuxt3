02840e4932bc66167e5f6792b9678a83b38787d5
yukimasaki <woodenhoe@gmail.com>
2023-08-23 01:38:13 +0000

    chore: zodを日本語化

--- src/seisan/plugins/veeValidate.ts
+++ src/seisan/plugins/veeValidate.ts
@@ -4,7 +4,13 @@ import ja from '@vee-validate/i18n/dist/locale/ja.json';
 import AllRules from '@vee-validate/rules';
 import { defineRule, configure } from 'vee-validate';
 
+import i18next from "i18next";
+import { z } from "zod";
+import { zodI18nMap } from "zod-i18n-map";
+import translation from "zod-i18n-map/locales/ja/zod.json";
+
 export default defineNuxtPlugin((nuxtApp) => {
+  // VeeValidateの言語設定
   configure({
     generateMessage: localize({
       en,
@@ -17,4 +23,13 @@ export default defineNuxtPlugin((nuxtApp) => {
   });
 
   setLocale('ja');
+
+  // zodの言語設定
+  i18next.init({
+    lng: "es",
+    resources: {
+      es: { zod: translation },
+    },
+  });
+  z.setErrorMap(zodI18nMap);
 });
