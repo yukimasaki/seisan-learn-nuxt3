60a498d40e7396b433911dcc1fbc54731773b325
yukimasaki <woodenhoe@gmail.com>
2023-09-26 02:05:32 +0000

    feat: findByPaymentDateに対応

--- src/seisan/composables/fetchDetail.ts
+++ src/seisan/composables/fetchDetail.ts
@@ -1,9 +1,14 @@
+import * as dayjs from 'dayjs';
 import { Detail } from '../types/detail';
 
-export const fetchDetail = async () => {
+export const fetchDetail = async (
+  start: string = dayjs().startOf('month').format('YYYY-MM-DD'),
+  end: string = dayjs().endOf('month').format('YYYY-MM-DD'),
+) => {
   const apiUrl = `http://seisan.local:3001`;
-  const { data }: { data: globalThis.Ref<Detail[]> } = await useFetch(`${apiUrl}/transaction`);
+  const { data }: { data: globalThis.Ref<Detail[]> } = await useFetch(`${apiUrl}/transaction?${start}&${end}`);
   const details = data;
+
   // 取得したデータをストアに格納する
   useState('details', () => details);
 }
