12c797d33c0f712de04fa847079337f881864f7f
yukimasaki <woodenhoe@gmail.com>
2023-09-26 01:49:38 +0000

    feat: { data }の型定義を追加

--- src/seisan/composables/fetchDetail.ts
+++ src/seisan/composables/fetchDetail.ts
@@ -2,7 +2,7 @@ import { Detail } from '../types/detail';
 
 export const fetchDetail = async () => {
   const apiUrl = `http://seisan.local:3001`;
-  const { data } = await useFetch(`${apiUrl}/transaction`);
+  const { data }: { data: globalThis.Ref<Detail[]> } = await useFetch(`${apiUrl}/transaction`);
   const details = data;
   // 取得したデータをストアに格納する
   useState('details', () => details);
