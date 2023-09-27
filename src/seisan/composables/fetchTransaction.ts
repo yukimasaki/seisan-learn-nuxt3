cbbf016c6db0aeaa3e6a0de340794a0f90b4c4d5
yukimasaki <woodenhoe@gmail.com>
2023-09-27 05:35:45 +0000

    feat: fetchTransactionへリネーム

---
+++ src/seisan/composables/fetchTransaction.ts
@@ -0,0 +1,10 @@
+import * as dayjs from 'dayjs';
+import { Transaction } from '../types/transaction';
+
+export const fetchTransaction = async (
+  start: string = dayjs().startOf('month').format('YYYY-MM-DD'),
+  end: string = dayjs().endOf('month').format('YYYY-MM-DD'),
+) => {
+  const apiUrl = `http://seisan.local:3001`;
+  const { data: details }: { data: Ref<Transaction[]> } = await useFetch(`${apiUrl}/transaction?start=${start}&end=${end}`);
+}
