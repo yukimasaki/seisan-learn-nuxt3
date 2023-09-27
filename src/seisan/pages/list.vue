5da4958844da05ed7596c1a105380dbdab8822b9
yukimasaki <woodenhoe@gmail.com>
2023-09-27 06:00:27 +0000

    feat: Calendarをコメントアウト

--- src/seisan/pages/list.vue
+++ src/seisan/pages/list.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-grow items-start justify-center">
     <div class="w-full bg-base-100">
       <div class="sticky top-16 z-20">
-        <Calendar></Calendar>
+        <!-- <Calendar></Calendar> -->
       </div>
       <Detail></Detail>
     </div>
