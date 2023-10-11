f6ff293c4bf60c86917a72d88413e92db3039ebc
yukimasaki <woodenhoe@gmail.com>
2023-10-11 02:38:33 +0000

    feat: 不要なadd関数を削除

--- src/seisan/components/footer.vue
+++ src/seisan/components/footer.vue
@@ -19,28 +19,8 @@
 </template>
 
 <script setup lang="ts">
-import dayjs from 'dayjs';
-import { Transaction } from '../types/transaction';
-
 const navigateToHome = () => {
   return navigateTo('/');
-}
-
-const add = () => {
-  const transaction: Transaction = {
-    id: 51,
-    categories: '分類不能',
-    amount: 9999,
-    paymentMethod: '金額指定',
-    paymentDate: dayjs(`2023-08-31`),
-    memo: 'added',
-    paidStatus: false,
-    createdAt: dayjs(`2023-08-31`),
-  }
-
-  const transactions: Ref<Transaction[]> = useState('transactions');
-  transactions.value.push(transaction);
-  transactions.value.sort((a, b) => b.createdAt.unix() - a.createdAt.unix());
 }
 
 const navigateToList = () => {
