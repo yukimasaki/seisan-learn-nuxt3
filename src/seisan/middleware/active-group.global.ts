51356e4f2829a5e4b757e205c228d09ab9b1dd23
yukimasaki <woodenhoe@gmail.com>
2023-10-12 01:42:23 +0000

    todo: メッセージをトーストで表示する

--- src/seisan/middleware/active-group.global.ts
+++ src/seisan/middleware/active-group.global.ts
@@ -10,8 +10,8 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   // ローカルストレージに永続化されている「アクティブなグループ」を、ストアに引っ張り出す (ActiveGroup | null)
   const activeGroupId: string | null = getLocalStorage('activeGroup');
 
-  // アクティブなグループを選択する必要があることを知らせるメッセージを表示する
-
+  // todo: メッセージをトーストで表示する
+  console.log(`アクティブなグループを設定してください`);
 
   // ストアがnullの場合は「アクティブなグループ」を選択する画面に遷移
   if (!activeGroupId && !excludePaths.includes(to.path)) return navigateTo('/setting');
